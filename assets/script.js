e8b8f834a993c6bc84a39b3e4ac530a78a9dd2fe
Keith Tharp <keithstharp@gmail.com>
2020-08-14 10:43:33 -0400

    Card switching complete.

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+const navLink = $("header");
+const aboutCard = $("#about-card");
+const contactCard = $("#contact-card");
+const portfolioCard = $("#portfolio-card");
+
+navLink.on("click", function (event) {
+  aboutCard.addClass("hide-card");
+  contactCard.addClass("hide-card");
+  portfolioCard.addClass("hide-card");
+  const link = `#${$(event.target).attr("id")}-card`;
+  $(link).removeClass("hide-card");
+
+});
